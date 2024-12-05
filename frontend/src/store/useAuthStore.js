b5ada659ced3dec0dad7725f7241ee97a2125996
ashishj12 <jangidashish38@gmail.com>
2024-12-05 19:30:10 +0530

    login and signup page

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,53 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import { toast } from "react-hot-toast";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isCheckingAuth: false,
+  isLoggingIn: false,
+  isSigningUp: false,
+
+  checkAuth: async () => {
+    set({ isCheckingAuth: true });
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data, isCheckingAuth: false });
+    } catch (error) {
+      set({ authUser: null, isCheckingAuth: false });
+    }
+  },
+  login: async (formData) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", formData);
+      set({ authUser: res.data, isLoggingIn: false });
+      toast.success("Login successful");
+    } catch (error) {
+      set({ isLoggingIn: false });
+      toast.error(error.response?.data?.message || "Login failed");
+    }
+  },
+
+  signUp: async (formData) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", formData);
+      set({ authUser: res.data, isSigningUp: false });
+      toast.success("Signup successful");
+    } catch (error) {
+      set({ isSigningUp: false });
+      toast.error(error.response?.data?.message || "Signup failed");
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error("Logout failed");
+    }
+  },
+}));
